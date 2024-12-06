87ff0039255f67b919e7a56cc19a2d127848cad1
Shadyman-creator <122512307+Shadyman-creator@users.noreply.github.com>
2024-12-07 00:26:56 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Product
+
+app = Flask(__name__)
+
+# Настройки базы данных
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///supermarket.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Инициализация базы данных
+db.init_app(app)
+
+@app.route('/')
+def product_list():
+    products = Product.query.all()
+    return render_template('list.html', products=products)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        name = request.form['name']
+        quantity = request.form['quantity']
+
+        # Создание нового товара
+        new_product = Product(name=name, quantity=int(quantity))
+        db.session.add(new_product)
+        db.session.commit()
+        
+        return redirect(url_for('product_list'))
+    return render_template('add.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=0)
